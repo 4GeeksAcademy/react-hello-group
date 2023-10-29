09e8c3325e412b727a688130414dbe131d558dfb
Daniel Pérez Arenas <144001482+Danielperezarenas@users.noreply.github.com>
2023-10-29 21:19:39 +0000

    feat: final commit

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const Footer = () => {
 
     return (
-        <div className="container-fluid bg-dark text-center mt-3" data-bs-theme="dark" style={{color: "white", fontSize: "15px"}}>
-                <p className="p-5">Copyright © Your Website 2023</p>
+        <div className="container-fluid bg-dark text-center mt-3" data-bs-theme="dark" style={{ color: "white", fontSize: "15px" }}>
+            <p className="p-5">Copyright © Your Website 2023</p>
         </div>
     );
 };
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-export const Navbar =  () => {
+export const Navbar = () => {
 
-return(
-<nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark shadow-sm">
-  <div className="container">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    
-    <div className="collapse navbar-collapse" style={{flexDirection : "row-reverse"}} id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark shadow-sm">
+      <div className="container">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" style={{ flexDirection: "row-reverse" }} id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Pricing</a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-)}
+    </nav>
+  )
+}
